c2331bbd284e88de54cea65d0d648cc2535024ca
saradepaulateixeira <saradepaulateixeira@gmail.com>
2024-04-03 16:11:21 -0300

    Ataulização em chamada de variaveis

---
+++ TrabalhandoNumericamente.py
@@ -0,0 +1,9 @@
+numero_1 = 5
+conversao_int = int(3.8)
+numero_2 = 3.14
+conversao_float = float(3)
+
+print(type(numero_1))
+print(type(conversao_int))
+print(type(numero_2))
+print(type(conversao_float))
