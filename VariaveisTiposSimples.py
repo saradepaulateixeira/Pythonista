5555df5f0b02ca94248a3c06ea44d553b296ac42
saradepaulateixeira <saradepaulateixeira@gmail.com>
2024-04-02 21:48:50 -0300

    uso de tipos simples

---
+++ VariaveisTiposSimples.py
@@ -0,0 +1,8 @@
+nome = "Sara"
+idade = "23"
+gosto_de = "Milho"
+
+print("Meu nome é " + nome + ".")
+print(f"Eu gosto de {gosto_de}")
+print("Eu tenho 23 anos")
+print("Sara gosta de programação e tem 23 anos")
