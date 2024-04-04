09011054c97a1aa3f00d424a780fb388650ab9e3
saradepaulateixeira <saradepaulateixeira@gmail.com>
2024-04-04 02:12:35 -0300

    raiz quadrada

--- FuncoesMatematicas.py
+++ FuncoesMatematicas.py
@@ -17,3 +17,10 @@ print(min(3,1,15,5,7,1))
 
 print("Arredondamento")
 print(round(3.9))
+
+print("Arredondamento para baixo")
+print(math.floor(3.9))
+print("Arredondamento para cima")
+print(math.ceil(3.9))
+print("Arredondamento raiz quadrada")
+print(math.sqrt(9))
