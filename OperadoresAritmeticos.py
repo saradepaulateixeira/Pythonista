82068d1124f714a746b14474364603154fc3b9fa
saradepaulateixeira <saradepaulateixeira@gmail.com>
2024-04-04 00:52:01 -0300

    Operando números

---
+++ OperadoresAritmeticos.py
@@ -0,0 +1,26 @@
+numero_um = 2
+numero_dois = 3
+
+print("soma:")
+print(numero_um + numero_dois)
+
+print("subtração:")
+print(numero_um - numero_dois)
+
+print("multiplicação:")
+print(numero_um * numero_dois)
+
+print("divisão:")
+print(numero_um / numero_dois)
+
+print("módulo:")
+print(numero_dois % numero_um)
+
+print("exponenciação:")
+print(numero_um ** numero_dois)
+
+print("Divisão inteira:")
+print(numero_dois // numero_um)
+
+print("Expressão numérica:")
+print((3 + 5) * (7 + 3))
