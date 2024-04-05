1934877b6b4a551898a77a79cd10a55dab70e829
saradepaulateixeira <saradepaulateixeira@gmail.com>
2024-04-05 02:07:17 -0300

    funções para manipular strings

---
+++ TrabalhandoStrings.py
@@ -0,0 +1,13 @@
+minha_string = "qualquer texto"
+
+print("impressão padrão")
+print(f"{minha_string}")
+
+print("impressão com função para caixa alta")
+print(minha_string.upper())
+
+print("impressão com função para caixa baixa")
+print(minha_string.lower())
+
+print("impressão com função para primeira letra com caixa alta")
+print(minha_string.capitalize())
